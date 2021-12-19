3ca30fe4fb5749685ef90a1c45094eaa4d6c6b3a
adnensaid <adnensaid83@gmail.com>
2021-12-19 21:09:05 +0100

    version 1.0

--- public/book.php
+++ public/book.php
@@ -9,66 +9,58 @@
     <title>Checkpoint PHP 1</title>
 </head>
 <body>
-
 <?php include 'header.php'; ?>
-
 <main class="container">
     <section class="desktop">
         <ul class="alphabetical">
-            <?php foreach(range('A', 'Z') as $letter) { ?>
-               <li><?= $letter; ?></li>
-            <?php } ?> 
+           <?php foreach(range('A', 'Z') as $letter) { ?>
+              <li><a href="<?= "?letter=".$letter; ?>"><?= $letter; ?></a></li>
+           <?php } ?> 
         </ul>
-        <div class="desktop-content">
-            <div class="whiskyImg">
-                <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
-                <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
-            </div>
-            <div class="pages">
-                <div class="page leftpage">
-                    Add a bribe
-                    <form action="" method="post">
-                        <div>
-                            <label for="name">name</label>
-                            <input type="text" name="name">
-                            <p style="color:red;margin:0;font-size:1.2rem;"><?= $errors['name'] ?? ''; ?></p>
-                        </div>
-                        <div>
-                            <label for="payment">payment</label>
-                            <input type="text" name="payment" required> 
-                            <p style="color:red;margin:0;font-size:1.2rem;"><?= $errors['payment'] ?? ''; ?></p>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Pay</button>
-                    </form>
-                </div>
-    
-                <div class="page rightpage">
-                    <!-- TODO : Display bribes and total paiement -->
-                    <div>
-                        <table>
-                           <thead>
-                              <tr>
-                                 <th>name</th>
-                                 <th>payment</th>
-                              </tr> 
-                           </thead>
-                           <tbody>
-                                <?php foreach($bribes as $bribe ){ ?>
-                                <tr>
-                                    <td> <?= $bribe['name']; ?> </td>
-                                    <td> <?= $bribe['payment']; ?>€</td>
-                                </tr>
-                                <?php  $total += $bribe['payment'];} ?>
-                           </tbody>
-                        </table>
-                        <tfoot>Total of payment : <?= $total; ?> € </tfoot>    
+        <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
+        <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
+        <div class="pages">
+            <div class="page leftpage">
+                <p>Add a bribe</p>
+                <form action="" method="post">
+                    <div class="mb-1">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" name="name" class="form-control">
+                        <ul class="error"><?= $errors['name'] ?? ''; ?></ul>
                     </div>
+                    <div class="mb-1">
+                        <label for="payment" class="form-label">Payment</label>
+                        <input type="text" name="payment" class="form-control"> 
+                        <ul class="error"><?= $errors['payment'] ?? ''; ?></ul>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary">Pay!</button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="page rightpage">
+                <!-- TODO : Display bribes and total paiement -->
+                <p class="title"><?= $_GET['letter'] ?? 'S'; ?></p>
+                <div>
+                    <table class="table">
+                       <tbody>
+                            <?php foreach($bribes as $bribe ){ ?>
+                            <tr>
+                                <td> <?= $bribe['name']; ?> </td>
+                                <td> <?= $bribe['payment']; ?>€</td>
+                            </tr>
+                            <?php  $total += $bribe['payment'];} ?>
+                            <tr class="total">
+                                <td>Total</td>
+                                <td><?= $total."€"; ?></td>
+                            </tr>
+                       </tbody>
+                    </table>
                 </div>
             </div>
-            <div class="inkpenImg">
-                <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>        
-            </div>
-        </div>        
+        </div>
+        <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>        
     </section>
 </main>
 </body>
